6f101b05048cb0958bb8a77b8ac22afb1a1f0ba3
Andres Crespo <acrerosj@canariaseducacion.es>
2025-11-05 15:12:52 +0000

    insert y archivo conexión

--- public/selectall.php
+++ public/selectall.php
@@ -1,50 +1,11 @@
 <?php
-// Página de prueba. Se debe eliminar de producción. 
-
-use Dotenv\Dotenv;
+// Tabla con todos los usuarios
 
 require_once '../vendor/autoload.php';
 
-// Leer variables de entono
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
-$host = $_ENV['DB_HOST'];
-//$host = '127.0.0.1';
-$db = $_ENV['DB_NAME'];
-//$db = 'tabla';
-$user = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
-//$password = '1234';
-$charset = $_ENV['DB_CHARSET'];
-
-// Hacer la conexión a la BD. 
-// Data Source Name (DSN)
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $password, $options);
-
-} catch (PDOException $e) {
-    echo "<h1>Error en la conexión</h1>";
-    printf("<p>%s</p>", $e->getMessage());
-    die();
-}
-
-//echo "Conexión correcta";
-
+require_once 'conexion.php';
 
 // Consulta SQL o manipulación del a base de datos.
-
-// Usuario por ID.
-$userId = '2';
 
 $sql = "SELECT id, name, email, register_date FROM users";
 $stmt = $pdo->prepare($sql);
@@ -72,6 +33,9 @@ $users = $stmt->fetchAll();
     </style>
 </head>
 <body>
+    <p>
+        <a href="create.php">Crear nuevo usuario</a>
+    </p>
     <table>
         <thead>
             <tr>

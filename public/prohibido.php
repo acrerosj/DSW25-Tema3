63bf9346294b5d3380ab819a722df1f09eb92348
Andrés Crespo <acrerosj@canariaseducacion.es>
2025-11-20 17:46:33 +0000

    logout y prohibido

---
+++ public/prohibido.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../bootstrap.php';
+
+$pageTitle = "Acceso prohibido";
+include '../includes/header.php';
+echo '<p>No tienes permiso para acceder a esta página</p>';
+include '../includes/footer.php';
